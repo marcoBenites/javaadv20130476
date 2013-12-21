81ec88ad32876de1b4bcdfbd800e5ecb055cc839
Nutria <egcarlos@gmail.com>
2013-12-21 10:21:39 -0500

    plantilla base para spring web

---
+++ spring-web/src/main/java/pe/edu/cibertec/javaadv/mod4/springweb/SpringStartupListener.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pe.edu.cibertec.javaadv.mod4.springweb;
+
+import javax.servlet.annotation.WebListener;
+import org.springframework.web.context.ContextLoaderListener;
+
+/**
+ *
+ * @author JAVA-ADV
+ */
+@WebListener
+public class SpringStartupListener extends ContextLoaderListener {
+}
